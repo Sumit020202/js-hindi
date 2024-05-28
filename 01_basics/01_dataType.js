51fe4df04602b6f87ad683b4b3fd995275b1cfd5
Sumit Singh <155011889+Sumit020202@users.noreply.github.com>
2024-05-28 21:54:00 +0000

    dataType

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,24 @@
+"use strict";// treat all js code as newer version
+// alert("hello")  we are using node js not browser
+console.log(3+
+    3) // code readability should be high
+
+console.log("sumit")
+
+let name = "sumit"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone  value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
