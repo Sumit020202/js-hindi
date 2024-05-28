5dd2214bed32a7c353f3409ddb23e6f88864413f
Sumit Singh <155011889+Sumit020202@users.noreply.github.com>
2024-05-28 21:30:35 +0000

    new file;

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sumit02@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2 not allowed
+accountEmail = "abc@gmail.com"
+accountPassword = "2324"
+accountCity = "Bangaluru"
+console.log(accountId);
+/*
+comment style
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
